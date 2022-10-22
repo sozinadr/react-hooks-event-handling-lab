f463b2d3d25904bc11d073f988eb2cf62e331b21
sozi <sozyarnadir@gmail.com>
2022-10-22 15:53:03 +0300

    Assignment Completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+    function handleChange() {
+        console.log("Entering password...");
+    }
+    return ( <
+        div >
+        <
+        input type = "password"
+        onChange = { handleChange } > < /input> <
+        /div>
+    );
 }
 
 export default Keypad;
